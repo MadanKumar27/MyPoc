ac5ace5dedce6c6d86d7650bf63c7bf669d8e77d
MadanKumar27 <MadanKumar27@users.noreply.github.com>
2015-09-02 12:31:22 -0400

    Update GITPocHelloWorkd.java

--- POC/src/com/poc/GITPocHelloWorkd.java
+++ POC/src/com/poc/GITPocHelloWorkd.java
@@ -6,6 +6,7 @@ public class GITPocHelloWorkd {
 		System.out.println("hai this this is a git Test");
 		System.out.println("test 2");
 		System.out.println("pushing just into master");
+		System.out.println("Changed in master);
 
 	}
 
